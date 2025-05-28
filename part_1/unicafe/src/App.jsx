b465a28b67bcffa08b8b4bf9729ef9ed1913d831
Anis Al-khatib <alkhatib.anis@gmail.com>
2025-05-29 01:22:04 +0300

    Complete Ex. 1.9 & Refactor Code

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,50 +1,74 @@
 import { useState } from "react";
 
 const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>{text}</button>
-  )
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Statistics = ({ type, value }) => {
+  if (type.toLowerCase().includes("rate")) {
+    return (
+      <p>
+        <strong>
+          <em>{type}</em>
+        </strong>
+        : {value}%
+      </p>
+    );
+  } else {
+    return (
+      <p>
+        <strong>
+          <em>{type}</em>
+        </strong>
+        : {value}
+      </p>
+    );
+  }
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+  // States
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const rateAsGood = () => setGood(good + 1)
-  const rateAsNeutral = () => setNeutral(neutral + 1)
-  const rateAsBad = () => setBad(bad + 1)
-  const total = sum(good, neutral, bad)
-  const goodRating = 1
-  const neutralRating = 0
-  const badRating = -1
-  const average = (good * goodRating + neutral * neutralRating + bad * badRating) / total
+  // Event Handlers
+  const rateAsGood = () => setGood(good + 1);
+  const rateAsNeutral = () => setNeutral(neutral + 1);
+  const rateAsBad = () => setBad(bad + 1);
+
+  // Variables
+  const total = good + neutral + bad;
+  const average = total !== 0 ? ((good - bad) / total).toFixed(2) : 0;
+  const positiveRate = total !== 0 ? ((good / total) * 100).toFixed(2) : 0;
+
+  // JSX
   return (
     <div>
       <h1>Give Feedback</h1>
       <Button text={"Good"} onClick={rateAsGood} />
       <Button text={"Neutral"} onClick={rateAsNeutral} />
       <Button text={"Bad"} onClick={rateAsBad} />
-      <h2>Statistics</h2>
-      <p><strong>Good</strong>: {good}</p>
-      <p><strong>Neutral</strong>: {neutral}</p>
-      <p><strong>Bad</strong>: {bad}</p>
-      <p><strong><em>Total</em></strong>: {total}</p>
-      <p><strong><em>Average</em></strong>: {total !== 0 ? average.toFixed(2) : 0}</p>
-      <p><strong><em>Positive</em></strong>: {total !== 0 ? (good / total * 100).toFixed(2) : 0}%</p>
+      <hr></hr>
+      {total === 0 ? (
+        <h2>
+          <em>No feedback yet . . .</em>
+        </h2>
+      ) : (
+        <>
+          <h2>Ratings</h2>
+          <Statistics type={"Good"} value={good} />
+          <Statistics type={"Neutral"} value={neutral} />
+          <Statistics type={"Bad"} value={bad} />
+          <hr></hr>
+          <h2>Statistics</h2>
+          <Statistics type={"Total Ratings"} value={total} />
+          <Statistics type={"Ratings Average"} value={average} />
+          <Statistics type={"Positive Feedback Rate"} value={positiveRate} />
+        </>
+      )}
     </div>
   );
 };
 
 export default App;
-
-function sum(...nums) {
-  let total = 0
-  for (const n of nums) {
-    if (typeof n === 'number' && !isNaN(n)) {
-      total += n
-    }
-  }
-  return total
-}
